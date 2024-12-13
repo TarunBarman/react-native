ea52740ff8a4e05dca4fe753e276e5f22870ac37
TarunBarman <tarunbarman9797@gmail.com>
2024-12-13 14:03:53 +0530

    view,button,text

---
+++ App.jsx
@@ -0,0 +1,18 @@
+/**
+ * @format
+ */
+
+
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize:30}}>App hello</Text>
+      <Button title='prease'/>
+    </View>
+  );
+};
+
+export default App;
